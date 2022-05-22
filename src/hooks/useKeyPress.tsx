54d771c787839821aeb6218755fd30485d005e2c
Cottonwood <blog_geon@naver.com>
2022-05-22 22:32:02 +0900

    eslint useEffect warning 제거

--- src/hooks/useKeyPress.tsx
+++ src/hooks/useKeyPress.tsx
@@ -18,6 +18,7 @@ export default function useKeyPress(targetKey: string): boolean {
       window.removeEventListener("keydown", downHandler);
       window.removeEventListener("keyup", upHandler);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return keyPressed;
 }
